4fe5885560ba4291ab3ca26a834dab85224f38c0
Bünyamin Yakut <80539363+shredderbns@users.noreply.github.com>
2021-04-21 11:18:39 +0300

    Add files via upload

---
+++ C#-secon-degree-equations/2.derece.denklem/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _2.derece.denklem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("A Değerini Giriniz = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("B Değerini Giriniz = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("C Değerini Giriniz = ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            double d = (b * b) - (4 * a * c);
+            if (d > 0)
+            {
+                double x1 = ((-b - Math.Sqrt(d)) / (2 * a));//math.sqrt ile karekökünü buluyorz
+                double x2 = ((-b + Math.Sqrt(d)) / (2 * a));
+                Console.WriteLine("İki kök var . x1= " + x1 + "x2= " + x2);
+              
+            }
+            else if (d == 0)
+            {
+                double x = -b / (2 * a);
+                Console.WriteLine("bir kök vardır = " + x);
+               
+            }
+            else
+            {
+                Console.Write("sanal kökdür");
+            }
+            Console.ReadKey();
+        }
+    }
+}
